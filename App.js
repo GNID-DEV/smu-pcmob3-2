bcc74e3ae1786046e183d00397133405eea21bae
Vince Ding <vince.ding@gmail.com>
2022-07-02 16:07:24 +0800

    Added screens

--- App.js
+++ App.js
@@ -1,72 +1,27 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React from "react";
+import { StyleSheet } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Entypo } from "@expo/vector-icons";
-
-import Constants from "expo-constants";
 import * as SQLite from "expo-sqlite";
+import NotesStack from "./screens/NotesStack";
+import AddScreen from "./screens/AddScreen";
 
-function NotesScreen({ navigation }) {
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <TouchableOpacity onPress={addNote} style={styles.headerIcon}>
-          <Entypo name="circle-with-plus" size={24} color="#BF8D7A" />
-        </TouchableOpacity>
-      ),
-    });
-  });
-
-  function addNote() {
-    console.log("Add Note");
-  }
-
-  return <View style={styles.container}></View>;
-}
-
+const db = SQLite.openDatabase("notes.db");
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Notes"
-          component={NotesScreen}
-          options={{
-            headerTitle: "NOTES APP",
-            headerTitleAlign: "center",
-            headerTitleStyle: {
-              fontWeight: "bold",
-              fontSize: 18,
-              color: "#BF8D7A",
-            },
-            headerStyle: {
-              height: 120,
-              backgroundColor: "#36403E",
-              borderBottomColor: "#A66A5D",
-              borderBottomWidth: 1,
-            },
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+ return (
+   <NavigationContainer>
+     <Stack.Navigator mode="modal" headerMode="none">
+       <Stack.Screen
+         name="Notes Stack"
+         component={NotesStack}
+         options={{ headerShown: false }}
+       />
+       <Stack.Screen name="Add Note" component={AddScreen} />
+     </Stack.Navigator>
+   </NavigationContainer>
+ );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#BF8D7A",
-    alignItems: "center",
-    justifyContent: "center",
-  },
 
-  headerIcon: {
-    marginRight: 30,
-    
-  }
-
-});
---
+++ screens/NotesScreen.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import {
+ StyleSheet,
+ Text,
+ View,
+ FlatList,
+ TouchableOpacity,
+} from "react-native";
+import { Entypo } from "@expo/vector-icons";
+
+export default function NotesScreen({ navigation }) {
+ const [notes, setNotes] = useState([
+   { title: "List 1", done: false, id: "0" },
+   { title: "List 2", done: false, id: "1" },
+   { title: "List 3", done: false, id: "2" },
+ ]);
+
+ useEffect(() => {
+   navigation.setOptions({
+     headerRight: () => (
+       <TouchableOpacity onPress={addNote}>
+         <Entypo
+           name="new-message"
+           size={24}
+           color="black"
+           style={{ marginRight: 20 }}
+         />
+       </TouchableOpacity>
+     ),
+   });
+ });
+
+ function addNote() {
+   navigation.navigate("Add Note");
+ }
+
+ function renderItem({ item }) {
+   return (
+     <View
+       style={{
+         padding: 10,
+         paddingTop: 20,
+         paddingBottom: 20,
+         borderBottomColor: "#ccc",
+         borderBottomWidth: 1,
+       }}
+     >
+       <Text style={{ textAlign: "left", fontSize: 16 }}>{item.title}</Text>
+     </View>
+   );
+ }
+
+ return (
+   <View style={styles.container}>
+     <FlatList
+       style={{ width: "100%" }}
+       data={notes}
+       renderItem={renderItem}
+     />
+   </View>
+ );
+}
+
+const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+   backgroundColor: "#ffc",
+   alignItems: "center",
+   justifyContent: "center",
+ },
+});
+
+
+
+
+
