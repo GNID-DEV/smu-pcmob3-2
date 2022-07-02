714cbe8f35e22d5cf103a2d5f92e27bff4a886c8
Vince Ding <vince.ding@gmail.com>
2022-07-02 14:47:16 +0800

    Implementation of Ui colour

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Entypo } from "@expo/vector-icons";
+
+import Constants from "expo-constants";
+import * as SQLite from "expo-sqlite";
+
+function NotesScreen({ navigation }) {
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={addNote} style={styles.headerIcon}>
+          <Entypo name="circle-with-plus" size={24} color="#BF8D7A" />
+        </TouchableOpacity>
+      ),
+    });
+  });
+
+  function addNote() {
+    console.log("Add Note");
+  }
+
+  return <View style={styles.container}></View>;
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Notes"
+          component={NotesScreen}
+          options={{
+            headerTitle: "NOTES APP",
+            headerTitleAlign: "center",
+            headerTitleStyle: {
+              fontWeight: "bold",
+              fontSize: 18,
+              color: "#BF8D7A",
+            },
+            headerStyle: {
+              height: 120,
+              backgroundColor: "#36403E",
+              borderBottomColor: "#A66A5D",
+              borderBottomWidth: 1,
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#BF8D7A",
+    alignItems: "center",
+    justifyContent: "center",
   },
+
+  headerIcon: {
+    marginRight: 30,
+    
+  }
+
 });
