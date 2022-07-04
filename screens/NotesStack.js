860bad8e9f4d57960171e7c4dbae4fc705c1b135
Vince Ding <vince.ding@gmail.com>
2022-07-04 21:27:34 +0800

    Version 1

--- screens/NotesStack.js
+++ screens/NotesStack.js
@@ -6,21 +6,21 @@ const InnerStack = createStackNavigator();
 export default function NotesStack() {
   return (
     <InnerStack.Navigator>
-      <InnerStack.Screen
-        name="Notes"
-        component={NotesScreen}
+      
+      <InnerStack.Screen name="Notes" component={NotesScreen}
         options={{
           title: "PCMOB 3 | NOTE APP",
+          
           headerTitleAlign: 'left',
           
           headerStyle: {
-            
             height: 120,
             backgroundColor: "#A63C58",
             shadowColor: "black",
             shadowOpacity: 0.8,
             shadowRadius: 3,
           },
+
           headerTintColor: "#D9BBB4",
           headerTitleStyle: {
             fontSize: 21,
